f85c9fdf4420841447e2f7b12e647644ab0645e1
zMaverick <gwhite29@caledonian.ac.uk>
2014-05-23 17:29:47 +0100

    Final Build
    Completed implementation of functionality:
    Takes input of data file ((directory can be specified with a variable)
    Stores real/imaginary numbers in an array of complex numbers
    Calculates the amplitude and phase values
    Outputs them to a data file that can also be specified with a variable

--- Coding Exercise/Coding Exercise/main.cpp
+++ Coding Exercise/Coding Exercise/main.cpp
@@ -12,14 +12,14 @@ int filesize (string dir)
 {
 	/* This function returns the total size of the file - GW */
 
-	ifstream inputFile(dir);		//Open the inputted file - GW
+	ifstream file(dir);		//Open the inputted file - GW
 	streampos begin, end;			//Variables to hold the beginning and end values - GW
 
-	begin = inputFile.tellg();		//Set the begin value - GW
-	inputFile.seekg (0, ios::end);	//Set the get positon to the end of the file - GW
-	end = inputFile.tellg();		//Set the end value - GW
+	begin = file.tellg();		//Set the begin value - GW
+	file.seekg (0, ios::end);	//Set the get positon to the end of the file - GW
+	end = file.tellg();		//Set the end value - GW
 	
-	inputFile.close();				//Close the file
+	file.close();				//Close the file
 	return end-begin;				//Return the size of the file - GW
 }
 
@@ -27,71 +27,67 @@ int rowcount (string dir)
 {
 	/* This function returns the total row count of the file - GW */
 
-	ifstream inputFile(dir);	//Open the inputted file - GW
+	ifstream file(dir);	//Open the inputted file - GW
 	string line;				//String for the getLine function - GW
 	int length = 0;				//The length variable to iterate upon - GW
 
 	/* Iterate for the length of the file - GW */
-	while (getline(inputFile, line))
+	while (getline(file, line))
 	{
 		++length;				//Increase the length variable per line - GW
 	}
 	
-	inputFile.close();			//Close the file - GW
+	file.close();			//Close the file - GW
 	
 	return length;				//Return the row count of the file - GW
 }
 
 int main ()
 {
-	string text;							//Temp string for testing file read - GW
+	string text;								//Temp string for holding lines from the file read - GW
 
-	string filedir = "";					//Use this for an external directory location	- GW
-	string filename = "reimadmt.dat";		//Name of the input file	- GW
+	string filedir = "";						//Use this for an external directory location	- GW
+	string inputName = "reimadmt.dat";			//Name of the input file	- GW
+	string outputname = "output.dat";			//Name of the output file	- GW
 
-	string theFile = filedir+filename;
-
-	ifstream file (theFile);				//Open the file with the full directory - GW
+	string inputFile = filedir+inputName;		//Full Directory of the input file - GW
+	string outputFile = filedir+outputname;		//Full Directory of the output file - GW
 	
-	int num_Columns = 2;					//As there are 2 columns (real/imaginary) - GW
-	int spacing = 2;						//The spacing-size between each column - GW
+	ifstream input (inputFile);					//Open the file with the full directory - GW
+	ofstream output;							//Variable for the output class - GW
+
+	int num_Columns = 2;						//As there are 2 columns (real/imaginary) - GW
+	int spacing = 2;							//The spacing-size between each column - GW
+	string delimiter = "  ";					//As two spaces are used to separate the columns set the delimiter to this - GW
 	
-	int size_File = filesize(theFile);			//Total size of the file - GW
-	int num_Rows = rowcount(theFile) + 1;		//The number of rows in the file (Extra row is hard-added due to an empty row at the bottom of which is being skipped) - GW
+	int size_File = filesize(inputFile);		//Total size of the file - GW
+	int num_Rows = rowcount(inputFile) + 1;		//The number of rows in the file (Extra row is hard-added due to an empty row at the bottom of which is being skipped) - GW
 	int num_PopulatedRows = num_Rows - 1;		//The number of populated rows in the file - GW
 
 	int num_Length = ((size_File/num_Rows+1) - spacing*2)/num_Columns;	//The calculated character size of the numbers (total size over total rows, remove spacing, over total columns) - GW
 
-	cout << "Number Length: " << num_Length << " Characters.\n";
-	cout << "Total Rows: " << num_Rows << ".\n";
-
-	double** numbers = new double*[num_PopulatedRows];		//Array of pointers to arrays, this will hold all the numbers - GW
-	complex<double>* numberTest = new complex<double>[num_PopulatedRows];
-
-
-	/* Populate the pointers array with doubles */
-	for (int i = 0; i < num_PopulatedRows; ++i)
-	{
-		numbers[i] = new double[num_Columns];
-	}
+	complex<double>* numberTest = new complex<double>[num_PopulatedRows];	//Array to store complex numbers - GW
 
 	/* Check the file is open */
-	if (file.is_open())
+	if (input.is_open())
 	{
-		cout << "File Contents:" << '\n';
+		/* Output for the user */
+		cout << "Input File Found: "
+			<< "'" << inputFile << "'" << endl
+			<< size_File << " Characters" << endl
+			<< num_Rows << " Rows" << endl;
 
-		file.seekg(spacing);			//Set the start position past the first spacing - GW
+		input.seekg(spacing);			//Set the start position past the first spacing - GW
 		int i = 0;						//Counter variable - GW
 		
 		/* Iterate while there is a line to read - GW */
-		while (getline(file, text))
+		while (getline(input, text))
 		{
 			/* Split text into 2 strings using the spacing and convert into a double - GW */
 
-			int pos = file.tellg();		//Get the current position - GW
-			file.seekg(pos+spacing);	//Set the positon to the current position plus the spacing - GW
+			int pos = input.tellg();		//Get the current position - GW
+			input.seekg(pos+spacing);	//Set the positon to the current position plus the spacing - GW
 						
-			string delimiter = "  ";	//As two spaces are used to separate the numbers set the delimiter to this - GW
 			size_t delimPos = 0;		//The position in the string of the delimiter - GW
 
 			/* Check if the delimiter is in the string - GW */
@@ -104,28 +100,42 @@ int main ()
 			double imaginary = atof((text.erase(0, delimPos + spacing)).c_str());	//Variable to hold imaginary number - GW
 			complex<double> tempComplex (real,imaginary);							//Create a temporary complex double - GW
 
-			numberTest[i] = tempComplex;											//Assign the temp complex double to the array - GW
+			numberTest[i] = tempComplex;			//Assign the temp complex double to the array - GW
 	
 			i++;		//iterate once per loop - GW
 		}
-		
-		file.close();	//When it's finished close the file - GW
+		input.close();	//When it's finished close the file - GW
 	}
 	else
 	{
 		/* Output error message otherwise - GW */
-		cout << "Error: Unable to open file";
+		cout << "Error: Failed to open file. Check the directory.";
 	}
 
-	/* Testing the Array - GW */
-	for(int i = 0; i < num_PopulatedRows ; ++i)
+	output.open(outputFile);		//Create the output file with the full directory - GW
+
+	if (output.is_open())
 	{
-		cout.precision(7);
-		cout << scientific;
-		cout << numberTest[i] << endl;
+		/* Outputting the Array - GW */
+		for(int i = 0; i < num_PopulatedRows ; ++i)
+		{
+			output.precision(7);			//Set the floating-point precision of the numbers to 7 - GW
+			output << scientific			//Numbers will be represented in scientific form - GW
+			<< delimiter					//First add the delimter - GW
+			<< abs(numberTest[i])			//Then the Amplitude in column 1 - GW
+			<< delimiter					//Then the second delimter to seperate the columns - GW
+			<< arg(numberTest[i]) << "\n";	//Finally add the Phase to column 2 and take a new line - GW
+		}
+		output.close();		//When it's finished close the file - GW
+		cout << "Output File Created: " << "'" << outputFile << "'" << endl;	//Output information for the user - GW
+	}
+	else
+	{
+		/* Output error message otherwise - GW */
+		cout << "Error: Failed to create file" << endl;
 	}
 
-	system("pause");		//Modified: Correct Implementation to keep output open upon run - GW
+	system("pause");		//Correct Implementation to keep output open upon run - GW
 	return 0;
 	
 }
